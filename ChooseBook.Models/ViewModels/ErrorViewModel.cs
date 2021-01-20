83f69d5a7632ce4e928e2197cea86e4ab85496d5
Jeremiah Ogunbor <jerryogunbor@gmail.com>
2021-01-20 01:12:24 +0100

    Add project files.

---
+++ ChooseBook.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChooseBook.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ChooseBook.Utility/SD.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChooseBook.Utility
+{
+   public static class SD
+    {
+    }
+}
